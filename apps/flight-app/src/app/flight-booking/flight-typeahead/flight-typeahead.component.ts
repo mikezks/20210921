04881310e1899f017ce5e8f4cfdc9849e949e305
michael.zikes <michael.zikes@intauria.com>
2021-09-22 09:57:13 +0200

    RxJS Custom Operators, UI Flag

--- apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -2,7 +2,7 @@ import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-lib';
-import { Observable, timer, EMPTY, Subscription, tap, share, map, of, debounceTime, distinctUntilChanged, filter, switchMap, withLatestFrom } from 'rxjs';
+import { Observable, timer, EMPTY, Subscription, tap, share, map, of, debounceTime, distinctUntilChanged, filter, switchMap, withLatestFrom, iif, pipe, catchError, UnaryFunction, combineLatestWith, startWith } from 'rxjs';
 
 @Component({
   selector: 'flight-workspace-flight-typeahead',
@@ -14,16 +14,64 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   subscription = new Subscription();
 
   control = new FormControl();
+  firstFlight = new FormControl();
   flights$: Observable<Flight[]> = of([]);
   loading = false;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.firstFlight.valueChanges.subscribe(console.log);
+
     const filterState = of({
       cityFilter: 'Wien',
       username:'michael.egger-zikes'
     });
+
+    const switchToState = <T, A>(
+      stateProvider: Observable<T>,
+      selector: (state: T) => A
+    ): UnaryFunction<Observable<T>, Observable<A>> => pipe(
+      // Stream 4: Filter State
+      // Data Provider
+      withLatestFrom(stateProvider),
+      map(([,state]: [unknown, T]) => selector(state))
+    );
+
+    const filterTypeahead = <T>(): UnaryFunction<
+      Observable<T>, Observable<T>
+    > => pipe(
+      // Filtering START
+      debounceTime(300),
+      distinctUntilChanged(),
+      // Filtering END
+    );
+
+    const loadFlights = (city: string) => of(city).pipe(
+      // Side-Effect: Assign class property "loading"
+      tap(() => this.loading = true),
+      // Stream 2: Switch to inner Observable to load data
+      // Data Provider
+      switchMap(city => this.load(city).pipe(
+        catchError(() => of([]))
+      )),
+      // Side-Effect: Assign class property "loading"
+      tap(() => this.loading = false)
+    );
+
+    const switchToConditionalStream = <T, A>(
+      condition: (state: T) => boolean,
+      streamA: (state: T) => Observable<A>,
+      streamB: (state: T) => Observable<A>
+    ) => pipe(
+      switchMap((state: T) =>
+        iif(
+          () => condition(state),
+          streamA(state),
+          streamB(state)
+        )
+      )
+    );
 
     // Stream 3: Result stream to render Flights
     this.flights$ =
@@ -33,20 +81,26 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       this.control.valueChanges.pipe(
         // Stream 4: Filter State
         // Data Provider
-        withLatestFrom(filterState),
-        map(([userFilter, { cityFilter }]) => userFilter),
-        // Filtering START
-        filter(city => city.length > 2),
-        debounceTime(300),
-        distinctUntilChanged(),
-        // Filtering END
-        // Side-Effect: Assign class property "loading"
-        tap(() => this.loading = true),
-        // Stream 2: Switch to inner Observable to load data
-        // Data Provider
-        switchMap(city => this.load(city)),
-        // Side-Effect: Assign class property "loading"
-        tap(() => this.loading = false)
+        /* switchToState(
+          filterState,
+          state => state.cityFilter
+        ), */
+        filterTypeahead(),
+        switchToConditionalStream(
+          city => city.length > 2,
+          // Stream 2: Switch to inner Observable to load data
+          // Data Provider
+          city => loadFlights(city),
+          // Stream 5: Reset to empty array
+          // Data Provider
+          () => of([])
+        ),
+        combineLatestWith(this.firstFlight.valueChanges.pipe(
+          startWith(false)
+        )),
+        map(([flights, showFirstFlight]) =>
+          flights.length && showFirstFlight ? [flights[0]] : flights
+        )
       );
 
     // this.rxjsDemo();
